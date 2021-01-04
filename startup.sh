53fd16d4aaa0b6f9ef9dbed4954db13c51edf2d9
CTaylor5299 <74771035+CTaylor5299@users.noreply.github.com>
2021-01-04 11:57:26 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-python3 -m pytest tests/test_unit.py --cov=application
+python3 -m pytest test/test_unit.py --cov=application
 
 python3 create.py
 python3 app.py
